fbef0df200587e3d78d0c82b348c109b50efe3d8
“hlwander” <hander@vt.edu>
2024-11-05 20:01:47 +0000

    starting sensitivity script

---
+++ analysis/zoop_par_sensitivity.R
@@ -0,0 +1,7 @@
+# sensitivity analysis for zoop pars that differ among taxa
+
+#load packages
+pacman::p_load(dplyr)
+
+# Rgrz_zoo (0.9-1.1), Rresp_zoo (0.2-0.3), Rmort_zoo (0.01-0.07), theta_resp_zoo (1.06-1.09)
+# note not doing prey preferences even though those differ among taxa
